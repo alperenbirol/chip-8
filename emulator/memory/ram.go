c06f67d3d33cb196b05db6b158162be73dceb28e
Alperen Birol <merhaba@alperenbirol.com>
2022-05-11 17:28:48 +0300

    Add load functions for memory

--- emulator/memory/ram.go
+++ emulator/memory/ram.go
@@ -6,8 +6,20 @@ type Memory struct {
 	ram emuconfig.Ram
 }
 
+func (m *Memory) loadAtAddress(address uint16, data []byte) {
+	copy(m.ram[address:], data)
+}
+
 func (m *Memory) UnloadROM() {
 	for i := 0x200; i < len(m.ram); i++ {
 		m.ram[i] = 0x00
 	}
+}
+
+func (m *Memory) LoadROM(rom []byte) {
+	m.loadAtAddress(0x200, rom)
+}
+
+func (m *Memory) LoadFonts(fonts []byte) {
+	m.loadAtAddress(0x50, fonts)
 }
