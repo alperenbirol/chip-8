c8610f60044143f487fdedd5a4ae553ed82a8630
Alperen Birol <merhaba@alperenbirol.com>
2022-05-12 00:14:26 +0300

    Registers implemented

--- emulator/decoder/decoder.go
+++ emulator/decoder/decoder.go
@@ -31,21 +31,33 @@ func (code *Opcode) Decode() instructions.Instruction {
 			switch nibbles[2] {
 			case 0xE:
 				switch nibbles[3] {
-				case 0x0:
+				case 0x0: // 00E0 - Clears the screen
 					return instructions.ClearScreen()
 				}
 			}
 		}
-	case 0x1: // 1nnn - JP addr
+	case 0x1: // 1NNN - Jumps program counter to address NNN
 		// convert 3 nibbles to a 16 bit address
 		address := uint16(nibbles[1])<<8 | uint16(nibbles[2])<<4 | uint16(nibbles[3])
 		return instructions.Jump(memory.Address(address))
-	case 0x6: // 6XNN: LD Vx, byte
-		// convert 2 nibbles to a 8 bit address
-		registerValue := byte(nibbles[2]<<4 | nibbles[3])
-		register := byte(nibbles[1])
-		// set VX(register) register to NN(registerValue)
-		return instructions.SetRegister(register, registerValue)
+	case 0x6: // 6XNN - Sets register X to NN
+		// convert 2 nibbles to a byte value
+		byteValue := byte(nibbles[2]<<4 | nibbles[3])
+		registerIndex := byte(nibbles[1])
+		// set VX(registerIndex) register to NN(byteValue)
+		return instructions.SetRegister(registerIndex, byteValue)
+	case 0x7: // 7XNN - Adds NN to register X
+		// convert 2 nibbles to a byte value
+		byteValue := byte(nibbles[2]<<4 | nibbles[3])
+		registerIndex := byte(nibbles[1])
+		// Add NN(byteValue) to VX(registerIndex)
+		return instructions.AddToRegister(registerIndex, byteValue)
+	case 0xA: // ANNN - Sets index register to NNN
+		// convert 3 nibbles to a 16 bit address
+		NNN := uint16(nibbles[1])<<8 | uint16(nibbles[2])<<4 | uint16(nibbles[3])
+		return instructions.SetIndexRegister(NNN)
+	case 0xD: // DXYN - Draws a sprite at coordinate (X,Y) with N bytes of sprite data
+
 	}
 
 	return nil
